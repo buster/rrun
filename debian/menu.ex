6b2d1319660e17861de6ed8d7fb8552881ce1e16
Sebastian Schulze <me@bstr.eu>
2014-12-11 13:48:59 +0100

    Added blank Debian package template

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(rrun):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="rrun" command="/usr/bin/rrun"
---
+++ debian/rrun.default.ex
@@ -0,0 +1,10 @@
+# Defaults for rrun initscript
+# sourced by /etc/init.d/rrun
+# installed at /etc/default/rrun by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
