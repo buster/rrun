7ca0adac717c1e809dbc80901a927e17ab0aed21
Sebastian Schulze <me@bstr.eu>
2016-01-07 19:57:11 +0100

    move creation of completers to function

--- src/main.rs
+++ src/main.rs
@@ -71,13 +71,9 @@ fn get_config_file() -> Result<File, String> {
 }
 
 
-
-#[allow(dead_code)]
-fn main() {
-    let mut file = get_config_file().unwrap();
-    // Read the file contents into a string, returns `io::Result<usize>`
+fn get_completers(config: &mut File) -> Vec<Box<autocomplete::AutoCompleter>> {
     let mut toml = String::new();
-    match file.read_to_string(&mut toml) {
+    match config.read_to_string(&mut toml) {
         Err(why) => panic!("couldn't read Configfile ~/.config/rrun/config.toml: {}",
                                                    Error::description(&why)),
         Ok(_) => (),
@@ -88,10 +84,17 @@ fn main() {
     let maybe_completions = value.get("completion").into_iter();
     let completions = maybe_completions.flat_map(|cs| cs.as_slice().unwrap().into_iter());
     let autocompleter_configs = completions.flat_map(|cs| cs.as_table());
-    let autocompleters:Vec<Box<AutoCompleter>> = autocompleter_configs.map(|cfg| {
+     autocompleter_configs.map(|cfg| {
         let command = cfg.get("command").and_then(|c| c.as_str()).map(|c| c.to_string()).unwrap();
         ExternalAutoCompleter::new(command)
-    }).collect();
+    }).collect()
+}
+
+#[allow(dead_code)]
+fn main() {
+    let mut file = get_config_file().unwrap();
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let autocompleters = get_completers(&mut file);
 
     gtk::init().unwrap_or_else(|_| panic!("Failed to initialize GTK."));
     debug!("Major: {}, Minor: {}", gtk::get_major_version(), gtk::get_minor_version());
